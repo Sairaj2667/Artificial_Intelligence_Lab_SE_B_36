0d46e96bd6df8c0edffdf0f2c0f440906ac485ef
Sairaj2667 <sairajkore16@gmail.com>
2025-09-25 10:15:43 +0530

    ALphaBetaPruning

---
+++ Artificial_Intelligence_Lab/alphabeta.py
@@ -0,0 +1,28 @@
+def alphabeta(node, depth, alpha, beta, maximizingPlayer, values, index=0):
+
+    if depth == 0 or index >= len(values):
+        return values[index]
+
+    if maximizingPlayer:
+        best = float('-inf')
+        for i in range(2):  
+            val = alphabeta(node*2+i, depth-1, alpha, beta, False, values, index*2+i)
+            best = max(best, val)
+            alpha = max(alpha, best)
+            if beta <= alpha:
+                break  
+        return best
+    else:
+        best = float('inf')
+        for i in range(2):
+            val = alphabeta(node*2+i, depth-1, alpha, beta, True, values, index*2+i)
+            best = min(best, val)
+            beta = min(beta, best)
+            if beta <= alpha:
+                break  
+        return best
+values = [3, 5, 6, 9, 1, 2, 0, -1]  
+depth = 3
+result = alphabeta(0, depth, float('-inf'), float('inf'), True, values)
+
+print("Optimal value (with Alpha-Beta Pruning):", result)
